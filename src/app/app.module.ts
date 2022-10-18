c1904f4e37c06b27886b34e02386c0bbff6c7ee5
Lotus Biswas <lotushotmail111@gmail.com>
2022-10-19 04:38:41 +0530

    testimonials, trainers, home ui added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { OurTrainersComponent } from './components/our-trainers/our-trainers.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    OurTrainersComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/our-trainers/our-trainers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-our-trainers',
+  templateUrl: './our-trainers.component.html',
+  styleUrls: ['./our-trainers.component.css']
+})
+export class OurTrainersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
